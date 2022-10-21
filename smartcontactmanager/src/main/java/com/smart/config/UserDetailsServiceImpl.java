a3c28aa1382f5e99bf6732a78066b5dce5096cd5
kimayaa-patil <115490623+kimayaa-patil@users.noreply.github.com>
2022-10-21 09:39:19 +0530

    Add files via upload

---
+++ smartcontactmanager/src/main/java/com/smart/config/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package com.smart.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import com.smart.entities.User;
+import com.smart.dao.UserRepository;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+
+	@Autowired
+	private UserRepository userRepository;
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// fetching data from databases
+		User user=userRepository.getUserByUserName(username);
+		
+		if(user==null) {
+			throw new UsernameNotFoundException("could not found user");
+		}
+		CustomUserDetails customUserDetails=new CustomUserDetails(user);
+		return customUserDetails;
+	}
+
+}
---
+++ smartcontactmanager/src/main/java/com/smart/controller/HomeController.java
@@ -0,0 +1,109 @@
+package com.smart.controller;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.smart.dao.UserRepository;
+import com.smart.entities.User;
+import com.smart.helper.Message;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@RequestMapping("/")
+	public String home(Model model) {
+		model.addAttribute("title", "Home - Smart Contact Manager");
+		return "home";
+	}
+	
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title", "about - Smart Contact Manager");
+		return "about";
+	}
+	
+	@RequestMapping("/contact")
+	public String contact(Model model) {
+		model.addAttribute("title", "Contact - Smart Contact Manager");
+		return "contact";
+	}
+	
+	@RequestMapping("/login")
+	public String login(Model model) {
+		model.addAttribute("title", "login - Smart Contact Manager");
+		return "login";
+	}
+	@RequestMapping("/signup")
+	public String signup(Model model) {
+		model.addAttribute("title", "signup - Smart Contact Manager");
+		model.addAttribute("user",new User());
+		return "signup";
+	}
+	
+	
+	// registration controller
+	@RequestMapping(value="/do_register" , method=RequestMethod.POST)
+	public String registerUser(@Valid @ModelAttribute("user") User user ,BindingResult result1, @RequestParam(value="agreement",defaultValue="false") boolean agreement,Model model, 
+						HttpSession session) {
+		try {
+			if(!agreement) {
+				System.out.print("You have not agreed the terms and condition");
+				throw new Exception("You have not agreed the terms and condition");
+			}
+			
+			if(result1.hasErrors()) {
+				System.out.println("ERROR" +result1.toString());
+				model.addAttribute("user", user);
+				return "signup";
+			}
+			System.out.println("aggre"+agreement);
+			System.out.println("user"+user);
+			
+			user.setRole("ROLE_USER");
+			user.setEnabled(true);
+			user.setImageUrl("default.png");
+			user.setPassword(passwordEncoder.encode(user.getPassword()));
+			
+			User result=this.userRepository.save(user) ;
+			
+			model.addAttribute("user",new User());
+			
+			session.setAttribute("message", new Message("Successfully registered !","alert-success"));
+			return "signup";
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user",user);
+			session.setAttribute("message", new Message("Something Went Wrong "+e.getMessage(),"alert-danger"));
+			return "signup";
+		}
+		
+	}
+	
+	
+	//handler for custom login
+	@GetMapping("/singin")
+	public String customLogin(Model model) {
+		model.addAttribute("title","Login Page");
+		return "login";
+	}
+}
